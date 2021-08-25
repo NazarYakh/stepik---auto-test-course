41f2b88800cfb0b37dd52c065677248f00b8b0cd
NazarYakh <nazaryakh@gmail.com>
2021-08-25 12:50:07 +0300

    solve some in lesson

--- explicit_wait2.py
+++ explicit_wait2.py
@@ -1,3 +1,10 @@
+import time
+import math
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
